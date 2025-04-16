8577c258371205ea5832ba28add2278e934ce343
ksyq12 <ksyq12@daum.net>
2025-04-16 15:14:27 +0900

    feat: 환율 모델 설계

--- ExchangeRateCalculator/ExchangeRateCalculator/Models/ExchangeRate.swift
+++ ExchangeRateCalculator/ExchangeRateCalculator/Models/ExchangeRate.swift
@@ -9,5 +9,10 @@ import Foundation
 
 struct ExchangeRateResult: Decodable {
     let timeLastUpdateUtc: String
-    let rates: [String: ]
+    let rates: [String: Double]
+    
+    enum CodingKeys: String, CodingKey {
+        case timeLastUpdateUtc = "time_last_update_utc"
+        case rates
+    }
 }
---
+++ ExchangeRateCalculator/ExchangeRateCalculator/Services/ExchangeRateService.swift
@@ -0,0 +1,12 @@
+//
+//  ExchangeRateService.swift
+//  ExchangeRateCalculator
+//
+//  Created by shinyoungkim on 4/16/25.
+//
+
+import Foundation
+
+final class ExchangeRateService {
+    
+}

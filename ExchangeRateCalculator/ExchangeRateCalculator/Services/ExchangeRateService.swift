0bcd31b95f6219c400b49a4f850b251e565faedb
ksyq12 <ksyq12@daum.net>
2025-04-16 15:23:40 +0900

    feat: 환율 데이터 요청, 디코딩 로직 구현

--- ExchangeRateCalculator/ExchangeRateCalculator/Services/ExchangeRateService.swift
+++ ExchangeRateCalculator/ExchangeRateCalculator/Services/ExchangeRateService.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 final class ExchangeRateService {
-    
+    func fetchData<T: Decodable>(url: URL, completion: @escaping (T?) -> Void) {
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, error == nil else {
+                print("데이터 로드 실패")
+                completion(nil)
+                return
+            }
+            let successRange = 200..<300
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+                    print("JSON 디코딩 실패")
+                    completion(nil)
+                    return
+                }
+                completion(decodedData)
+            } else {
+                print("응답 오류")
+                completion(nil)
+            }
+        }.resume()
+    }
 }

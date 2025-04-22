71d22b3f71fb19a5d69879d058675487e5a11c69
ksyq12 <ksyq12@daum.net>
2025-04-22 14:50:45 +0900

    feat: CoreData 설정

--- ExchangeRateCalculator/ExchangeRateCalculator/AppDelegate.swift
+++ ExchangeRateCalculator/ExchangeRateCalculator/AppDelegate.swift
@@ -21,5 +21,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ExchangeRateCalculator")
+        container.loadPersistentStores(completionHandler: { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError( "Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
---
+++ ExchangeRateCalculator/ExchangeRateCalculator/Models/CoreData/FavoriteCurrency+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteCurrency+CoreDataClass.swift
+//  ExchangeRateCalculator
+//
+//  Created by shinyoungkim on 4/22/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavoriteCurrency)
+public class FavoriteCurrency: NSManagedObject {
+
+}
